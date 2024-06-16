589c7f7ed9c7a27e16d2a093a37560aa7a29b1bc
AdiSabban10 <adialon050@gmail.com>
2024-06-16 16:30:01 +0300

    Add backend

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
-// app.listen(3030, () => console.log(`Server listening on port http://127.0.0.1:3030/`))
+// app.use(express.static('public'))
+app.use(cookieParser())
+
+// Express Routing:
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+	    .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error(`Couldn't get bugs...`)
+            res.status(500).send(`Couldn't get bugs...`)
+        })
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const { _id, title, description, severity ,createdAt } = req.query
+  const bugToSave = {
+    _id,
+    title,
+    description,
+    severity: +severity,
+    createdAt: +createdAt,
+  }
+
+    bugService.save(bugToSave)
+	    .then(savedBug => res.send(savedBug))
+})
+
+app.get('/api/bug/:id', (req, res) => {
+    const { id } = req.params
+
+    bugService.getById(id)
+        .then(bug => res.send(bug))
+})
+
+app.get('/api/bug/:id/remove', (req, res) => {
+    const { id } = req.params
+
+    bugService.remove(id)
+        .then(() => res.send(`Bug ${id} deleted...`))
+})
+
+
+const port = 3030
+app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))
+// app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:3030/`))
